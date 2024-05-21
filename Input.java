a1521f0a1307c3bd16e054f3c144a8a157aa1214
Taha Zoabi <zoabi03@gmail.com>
2024-05-21 23:02:45 +0300

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,23 @@
+package TicTacToe;
+
+import java.util.Scanner;
+
+public class Input {
+   static Scanner  input = new Scanner(System.in);
+
+    public static int[] playerChoice(char[][] board) {
+        int row, col;
+        while (true) {
+            System.out.print("Enter your move (row and column): ");
+            row = input.nextInt();
+            col = input.nextInt();
+            if (row >= 0 && row < 3 && col >= 0 && col < 3) {
+                break;
+            }
+            else {
+                System.out.println("This is not a valid move. Please enter row and column between 0 and 2.");
+            }
+        }
+        return new int[]{row, col};
+    }
+}
