652006317ef8c6223517d983a50bfd0c3b990b22
Taha Zoabi <zoabi03@gmail.com>
2024-05-22 11:18:28 +0300

    Update Input.java

--- Input.java
+++ Input.java
@@ -3,7 +3,7 @@ package TicTacToe;
 import java.util.Scanner;
 
 public class Input {
-   static Scanner  input = new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);
 
     public static int[] playerChoice(char[][] board) {
         int row, col;
@@ -11,11 +11,10 @@ public class Input {
             System.out.print("Enter your move (row and column): ");
             row = input.nextInt();
             col = input.nextInt();
-            if (row >= 0 && row < 3 && col >= 0 && col < 3) {
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '_') {
                 break;
-            }
-            else {
-                System.out.println("This is not a valid move. Please enter row and column between 0 and 2.");
+            } else {
+                System.out.println("This is not a valid move. Please enter row and column between 0 and 2, and ensure the spot is empty.");
             }
         }
         return new int[]{row, col};
