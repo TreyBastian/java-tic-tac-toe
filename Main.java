8a2abea3c966aebbeac45e39b6551f294490327f
Taha Zoabi <zoabi03@gmail.com>
2024-05-22 11:18:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ package TicTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        Game TicTacToe = new Game();
-        System.out.println("Let's play a game of Tic-Tac-Toe!");
-        char[][] gameBoard = TicTacToe.createBoard();
-        TicTacToe.printBoard(gameBoard);
-        TicTacToe.playerTurn();
+        Game ticTacToe = new Game();
+        Input choice = new Input();
+        ticTacToe.createBoard();
+        ticTacToe.printBoard(ticTacToe.board);
+        ticTacToe.playerTurn(choice);
     }
 }
